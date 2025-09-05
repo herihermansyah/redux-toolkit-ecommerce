421321745f812ea38bc59bf8b8208c216f07218a
heri hermansyah <54703307+herihermansyah@users.noreply.github.com>
2025-09-06 04:48:14 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), tailwindcss()],
-  base: mode === "production" ? "/redux-toolkit-ecommerce/" : "/",
+  plugins: [react(), tailwindcss()]
 }));
